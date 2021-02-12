ea734edfa1e54facfc59797bb705a4f7ad33eb78
Ainar Kukin <ainar.kukin@khk.ee>
2021-02-12 08:53:17 +0000

    Tegin juurde kasutajad

--- 2. teema/Restonan.py
+++
@@ -1,8 +0,0 @@
-class Restoran():
-    restorani_nimi = ""
-    soogi_tyyp = ""
-    def restoraani_kirjeldus(self):
-        print("Restoraan " + str(self.restorani_nimi) + ", söögi tüüp " + str(self.soogi_tyyp))
-    def ava_restoraan(self):
-        print("Restoraan on avatud")
-    def
---
+++ 2. teema/Restoraan.py
@@ -0,0 +1,7 @@
+class Restoraan():
+    restoraani_nimi = ""
+    soogi_tyyp = ""
+    def restoraani_kirjeldus(self):
+        print("Restoraan " + str(self.restoraani_nimi) + ", söögi tüüp " + str(self.soogi_tyyp))
+    def ava_restoraan(self):
+        print("Restoraan on avatud")
