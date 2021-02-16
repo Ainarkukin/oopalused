f279673fa4c56aca8a589da701a80f4c57f1a54b
Ainar Kukin <ainar.kukin@khk.ee>
2021-02-16 07:10:56 +0000

    Tegin juurde autode ülesande atribuudi

--- 3.teema/3,2kasutajad.py
+++ 3.teema/3,2kasutajad.py
@@ -3,3 +3,4 @@ from  auto import Auto
 ainari_uus_auto = Auto( "Audi", "A6" , 2017)
 
 print(ainari_uus_auto.kirjeldus())
+ainari_uus_auto.odomeeter()
--- 3.teema/auto.py
+++ 3.teema/auto.py
@@ -3,6 +3,11 @@ class Auto():
         self.tootja = t
         self.mudel = m
         self.aasta = a
+        self.odomeeteri_nait = 0
+
     def kirjeldus(self):
         pikk_nimi = str(self.aasta) + " " + self.tootja + self.mudel
         return pikk_nimi.title()
+
+    def odomeeter(self):
+        print("Antud auto sõitnud läbi " + str(self.odomeeteri_nait) + "km.")
