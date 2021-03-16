37a7df9b07bdc86f8c6674aa2fa6834f2189758a
Ainar Kukin <ainar.kukin@khk.ee>
2021-03-16 09:20:03 +0000

    Ristkylikud

---
+++ Mingid asjad/Ristkylikud.py
@@ -0,0 +1,23 @@
+class Ristkülik:
+    def __init__(self, laius, korgus, symbol):
+        self.width = int(laius)
+        self.height = int(korgus)
+        self.symbol = str(symbol)
+    def __str__(self):
+        ruut = []
+        for i in range(self.height):
+            ruut.append(self.symbol * self.width)
+        ruut = '\n'.join(ruut)
+        return ruut
+    def __add__(self, x):
+        self.width = self.width + x.width
+        self.height = self.height + x.height
+        self.symbol = self.symbol
+
+kujund1 = Ristkülik(10, 3, '*')
+print(kujund1)
+kujund2 = Ristkülik(8, 3, 'z')
+print(kujund2)
+
+kujund1.__add__(kujund2)
+print(kujund1)
